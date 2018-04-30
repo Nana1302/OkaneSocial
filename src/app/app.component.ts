8766e3a564207968cce92b22c0dc89a1e82a1011
nana hamzah <suehana_96@yahoo.com>
2018-04-30 16:49:39 +0800

    ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,45 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { Component, ViewChild } from '@angular/core';
+import { Nav, Platform } from 'ionic-angular';
 
 import { HomePage } from '../pages/home/home';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
+  @ViewChild(Nav) nav: Nav;
   rootPage:any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
-    platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
-      splashScreen.hide();
-    });
+  pages: Array<{title: string, component: any,}>;
+
+  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
+
+    this.pages = [
+      { title: 'Payment', component: HomePage },
+      // { title: 'Your Trades', component: HomePage },
+      // { title: 'Rewards', component: HomePage },
+      // { title: 'Help', component: HomePage },
+      // { title: 'Settings', component: HomePage },
+      // { title: 'Legal', component: HomePage }
+   
+    ];
+
   }
+
+  // initializeApp() {
+  //   this.platform.ready().then(() => {
+  //     // Okay, so the platform is ready and our plugins are available.
+  //     // Here you can do any higher level native things you might need.
+  //     this.statusBar.styleDefault();
+  //     this.splashScreen.hide();
+  //     });
+  // }
+
+  openPage(page) {
+    this.nav.setRoot(page.component);
+  }
+ 
 }
+
 
